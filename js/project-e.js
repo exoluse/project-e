f810282122bfbf52170cf912093bcdd521fe91df
exoluse <exoluse@hotmail.com>
2015-05-01 11:32:57 +0900

    커밋!!
    값 시작검색, 엔드검색, like 검색 추가

--- js/project-e.js
+++ js/project-e.js
@@ -79,7 +79,65 @@ selectCheckboxIntRange: function(name, val1, val2){
 			selectedObj[i].checked = true;
 		}
 	}
+},
+
+
+/*
+selectCheckboxStrStartAt
+*/
+selectCheckboxStrStartAt: function(name, val){
+	
+	var selectedObj = document.getElementsByName(name);
+	var valLength = val.toString().length; // 스트링으로 파싱해줘야 한다.
+	
+	for(var i=0;i<selectedObj.length;i++){
+
+		if(selectedObj[i].value.substring(0, valLength)==val){
+			selectedObj[i].checked = true;
+		}
+	}
+},
+
+
+/*
+selectCheckboxStrEndTo
+*/
+selectCheckboxStrEndTo: function(name, val){
+	
+	var selectedObj = document.getElementsByName(name);
+	var valLength = val.toString().length; // 스트링으로 파싱해줘야 한다.
+	
+	
+	for(var i=0;i<selectedObj.length;i++){
+
+		if(selectedObj[i].value.substring(selectedObj[i].value.length-valLength, selectedObj[i].value.length)==val){
+			selectedObj[i].checked = true;
+		}
+	}
+},
+
+
+/*
+selectCheckboxStrLike
+*/
+selectCheckboxStrLike: function(name, val){
+	
+	var selectedObj = document.getElementsByName(name);
+	var valLength = val.toString().length; // 스트링으로 파싱해줘야 한다.
+
+	for(var i=0;i<selectedObj.length;i++){
+
+		for(var j=0;j<selectedObj[i].value.toString().length-1;j++){
+
+			if(selectedObj[i].value.substring(j, valLength+j)==val){
+				selectedObj[i].checked = true;
+			}
+		}
+	}
 }
+
+
+
 
 
 //////////////// end
